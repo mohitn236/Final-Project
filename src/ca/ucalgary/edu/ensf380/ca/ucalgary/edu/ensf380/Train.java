dcfd2563a87c1f35ca6e8bfdf5a76edfffd2dff9
Shivansh Sharma <shivansh.sharma@ucalgary.ca>
2024-07-30 20:14:16 -0600

    Adding train class

---
+++ src/ca/ucalgary/edu/ensf380/ca/ucalgary/edu/ensf380/Train.java
@@ -0,0 +1,37 @@
+package ca.ucalgary.edu.ensf380;
+
+public class Train {
+    private int id;
+    private String direction; // "forward" or "backward"
+    private int speed; // Stations per interval
+
+    public Train(int id, String direction, int speed) {
+        this.id = id;
+        this.direction = direction;
+        this.speed = speed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
